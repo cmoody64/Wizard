ae7a6486b41754ea076db60b9e5794f290f46ffd
Connor Moody <cdmoody0604@gmail.com>
2018-05-18 19:13:32 -0600

    added wizard card and wizard deck classes

---
+++ Wizard/Card.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wizard
+{
+    enum CardValue
+    {
+        JESTER = 1,
+        TWO,
+        THREE,
+        FOUR,
+        FIVE,
+        SIZE,
+        SEVEN,
+        EIGHT,
+        NINE,
+        TEN,
+        JACK,
+        QUEEN,
+        KING,
+        ACE,
+        WIZARD
+    }
+
+    enum CardSuite
+    {
+        SPADES,
+        CLUBS,
+        HEARTS,
+        DIAMONDS,
+        SPECIAL
+    }
+
+    class Card
+    {
+        public Card(CardValue value, CardSuite suite)
+        {
+            Value = value;
+            Suite = suite;
+        }
+
+        public CardValue Value { get; }
+        public CardSuite Suite { get; }
+    }
+}
---
+++ Wizard/Deck.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wizard
+{
+    class Deck
+    {
+        // populates cards list with fixed Wizard deck
+        public Deck()
+        {
+            var cards = new List<Card>();
+            var standardSuites = new List<CardSuite> { CardSuite.CLUBS, CardSuite.SPADES, CardSuite.HEARTS, CardSuite.DIAMONDS };
+
+            // add in TWO to ACE in each suite besides special
+            foreach(var cardVal in Enumerable.Range((int)CardValue.TWO, (int)CardValue.ACE))
+            {
+                foreach(var cardSuite in standardSuites)
+                {
+                    cards.Add(new Card((CardValue)cardVal, cardSuite));
+                }
+            }
+
+            // add in special cards
+            for(int i = 0; i < NUM_SPECIAL_CARDS; i++)
+            {
+                cards.Add(new Card(CardValue.JESTER, CardSuite.SPECIAL));
+                cards.Add(new Card(CardValue.WIZARD, CardSuite.SPECIAL));
+            }
+
+            Cards = cards;
+        }
+
+        public IReadOnlyList<Card> Cards { get; }
+
+        private readonly int NUM_SPECIAL_CARDS = 4;
+    }
+}
